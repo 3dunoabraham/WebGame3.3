ddd3e9e7130f2c07be4e9d50f96c66b7b334302b
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-04-30 01:33:44 -0400

    reident

--- src/model/level/level2/BuyLowSellHigh.tsx
+++ src/model/level/level2/BuyLowSellHigh.tsx
@@ -5,33 +5,38 @@ import { MeshStandardMaterial } from 'three';
 
 
 function Component ({}) {
-    const $textGroup:any = useRef()
+  const $textGroup:any = useRef()
 
-    useFrame((ctx, delta)=>{
-        if (!$textGroup.current) return
+  useFrame((ctx, delta)=>{
+    if (!$textGroup.current) return
 
-        $textGroup.current.position.y = Math.sin(Date.now()/500)/10-0.39
-    })
+    $textGroup.current.position.y = Math.sin(Date.now()/500)/10-0.39
+  })
 
-    return (<group ref={$textGroup} position={[0.3,0,0.5]} rotation={[-0.5,0,0]}
-
-    >
-        <Text position={[0.2,0.28,0.6]} fontSize={0.3} rotation={[0,Math.PI,0]} 
-            material={new MeshStandardMaterial({ side: 0, color: "#005500" })}>
-                Click the button
-        </Text>
-        <Text position={[-0.2,0.28,0.6]} fontSize={0.3} rotation={[0,0,0]} 
-            material={new MeshStandardMaterial({ side: 0, color: "#005500" })}>
-                Click the button
-        </Text>
-        <Text position={[-0.,0,0.6]} fontSize={0.35} rotation={[0,Math.PI,0]} 
-            material={new MeshStandardMaterial({ side: 0, color: "#cc00cc" })}>
-                to buy
-        </Text>
-        <Text position={[0.,0,0.6]} fontSize={0.35} rotation={[0,0,0]} 
-            material={new MeshStandardMaterial({ side: 0, color: "#cc00cc" })}>
-                to buy
-        </Text>
-    </group>)
+  return (
+    <group ref={$textGroup} position={[0.3,0,0.5]} rotation={[-0.5,0,0]}>
+      <Text position={[0.2,0.28,0.6]} fontSize={0.3} rotation={[0,Math.PI,0]} 
+        material={new MeshStandardMaterial({ side: 0, color: "#005500" })}
+      >
+          Click the button
+      </Text>
+      <Text position={[-0.2,0.28,0.6]} fontSize={0.3} rotation={[0,0,0]} 
+        material={new MeshStandardMaterial({ side: 0, color: "#005500" })}
+      >
+          Click the button
+      </Text>
+      <Text position={[-0.,0,0.6]} fontSize={0.35} rotation={[0,Math.PI,0]} 
+        material={new MeshStandardMaterial({ side: 0, color: "#cc00cc" })}
+      >
+        to buy
+      </Text>
+      <Text position={[0.,0,0.6]} fontSize={0.35} rotation={[0,0,0]} 
+        material={new MeshStandardMaterial({ side: 0, color: "#cc00cc" })}
+      >
+        to buy
+      </Text>
+    </group>
+  )
 }
+
 export default Component
